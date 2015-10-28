f93afcc831ce5b8c3f347e8059723576faf043f7
akhr <akhashr@gmail.com>
2015-10-28 16:20:43 -0700

    Working D3 real-time area chart with Math.random() with update freq 1 sec - Redraws graph, axes and re-scales every 1 sec

--- dataVisualization/d3/firstCharts/scripts/simpleLineGraph_new.js
+++ dataVisualization/d3/firstCharts/scripts/simpleLineGraph_new.js
@@ -1,30 +1,36 @@
 var margin = {
-    top: 20,
-    right: 20,
-    bottom: 20,
-    left: 20
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
 };
 
 var chartMode;
 var width;
 var height;
-var dataSet;
+var datasetLength = 180;
+var dataset;
 var xScale;
 var yScale;
 var xAxis;
 var yAxis;
 var area;
 var graph;
-var interpolationType = "basis";
+var path;
+var interpolationType = "linear";
+var transitionDelay = 500;
 
 function prepareChart(mode, parentWidth, parentHeight) {
     this.chartMode = mode;
     defineChartDimension(parentWidth, parentHeight);
-    this.dataSet = generateInitChartData();
+    this.dataset = generateInitChartData();
     defineGraph();
     defineScaling();
     defineAxes();
     defineAreaPathGenerator();
+    addAxisToGraph();
+    addPathGeneratorToGraph();
+    generateMockChartData();
 }
 
 function defineChartDimension(parentWidth, parentHeight) {
@@ -36,63 +42,82 @@ function defineChartDimension(parentWidth, parentHeight) {
 
 function generateInitChartData() {
     var initDataArray = [];
-    for (var i = 0; i < 180; i++) {
+    for (var i = 0; i < datasetLength; i++) {
         var dataPoint = {};
         dataPoint.total = 0;
         dataPoint.compression = 0;
-        dataPoint.throughput = null;
+        dataPoint.throughput = Math.round(Math.random() * 40 + 10);
         initDataArray.push(dataPoint);
     }
+    printDataset(initDataArray)
     return initDataArray;
 }
 
 function defineScaling() {
     xScale = d3.scale.linear()
-        .domain([0, dataSet.length])
+        .domain([0, dataset.length])
         .range([0, width]);
 
     yScale = d3.scale.linear()
-        .domain(d3.extent(dataSet, function(d) {
+        .domain(d3.extent(dataset, function(d) {
             return d.throughput;
         }))
         .range([height, 0]);
 }
 
 function defineAxes() {
+    defineXAxis();
+    defineYAxis();
+}
+
+function defineXAxis() {
     xAxis = d3.svg.axis()
         .scale(xScale)
+        .tickFormat(function(d, i){
+            console.log("X axis tick formatting called - i ="+i);
+            if(d == 60)
+                return "2mins ago";
+            else if(d == 120)
+                return "1min ago";
+            else
+                null;
+        })
         .tickSubdivide(false)
         .orient("bottom");
+}
 
+function defineYAxis() {
     yAxis = d3.svg.axis()
         .scale(yScale)
         .ticks(6)
         .orient("left");
+
 }
 
 function defineAreaPathGenerator() {
     area = d3.svg.area()
         .x(function(d, i) {
-            console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+            // console.log('Plotting X value for data point: ' + d.throughput + ' using index: ' + i + ' to be at: ' + xScale(i) + ' using our xScale.');
             return xScale(i);
         })
         .y0(function(d) {
             return height;
         })
         .y1(function(d) {
-            console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+            // console.log('Plotting Y value for data point: ' + d.throughput + ' to be at: ' + yScale(d.throughput) + " using our yScale.");
             return yScale(d.throughput);
         })
-        .intepolate(interpolationType);
+        .interpolate(interpolationType);
 }
 
 function defineGraph() {
     graph = d3.select("#inboundChart")
         .append("svg:svg")
-        .attr("width", width)
-        .attr("height", height)
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("svg:g")
-        .attr("transform", "translate(" + margin["left"] + "," + margin["top"] + ")");
+        .attr("transform", "translate(" + (margin["left"]) + "," + margin["top"] + ")")
+        .style("fill", "blue");
 }
 
 
@@ -109,17 +134,32 @@ function addAxisToGraph() {
 }
 
 function addPathGeneratorToGraph() {
-    graph.append("svg:path")
-        .attr("d", area(data))
+    path = graph.append("svg:path")
+        .attr("d", area(dataset))
         .attr("class", "area");
 }
 
 
+var loopCounter = 0;
+
 function generateMockChartData() {
+    // console.log("generateMockChartData() called");
+
+    /*if (loopCounter >= 5) {
+        console.log("Exiting mock data generation");
+        printDataset(dataset);
+        redrawChart()
+        return;
+    }*/
+
+    loopCounter++;
     var dataPoint = {};
-    dataPoint.total = Math.round(Math.random() * 40 + 10);;
-    dataPoint.compression = Math.round(Math.random() * 40 + 10);;
-    dataPoint.throughput = Math.round(Math.random() * 40 + 10);;
+    dataPoint.total = Math.round(Math.random() * 40 + 10);
+    dataPoint.compression = Math.round(Math.random() * 40 + 10);
+    var min = 0;
+    var max = 400;
+    dataPoint.throughput = Math.round(Math.random() * (max - min) + min);
+    console.log("Generated Throughput : " + dataPoint.throughput);
     this.pushDataPoint(dataPoint);
     setTimeout(function() {
         generateMockChartData();
@@ -127,24 +167,42 @@ function generateMockChartData() {
 }
 
 function pushDataPoint(dataPoint) {
-    if (dataSet) {
-        if (dataSet.length < 180) {
-            dataSet.push(dataPoint);
-        } else if (dataSet.length === 180) {
-            dataSet.shift();
-            dataSet.push(dataPoint);
+    // console.log("pushDataPoint() called");
+    if (dataset) {
+        if (dataset.length < datasetLength) {
+            dataset.push(dataPoint);
+        } else if (dataset.length === datasetLength) {
+            dataset.shift();
+            dataset.push(dataPoint);
         }
     }
     redrawChart();
 }
 
-function redrawChart(){
-    graph.selectAll("path")
+function redrawChart() {
+    console.log("redrawChart() called");
+    defineScaling();
+    defineYAxis()
+    graph.selectAll(".y.axis")
+        .call(yAxis);
+
+    path
         .data([dataset])
-        .attr("transform", "translate("+ xScale(1) +")")
-        .attr("d", area)
+        .attr("transform", "translate(" + xScale(0) + ")")
+        .attr("d", area(dataset))
         .transition()
         .ease("linear")
-        .duration(transistionDelay)
-        .attr("transform", "translate("+ xScale(0) +")")
+        .duration(transitionDelay)
+        .attr("transform", "translate(" + xScale(0) + ")");
 }
+
+function printDataset(dataArray) {
+    console.log("Printing DATAsET");
+    for (i = 0; i < dataArray.length; i++) {
+        console.log("i=" + i + " " + dataArray[i].throughput);
+    }
+    console.log("DONE Printing DATASET");
+}
+
+
+prepareChart("", 640, 320);
